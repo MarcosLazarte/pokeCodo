df3156bbbc8f37c79f14004675733e0022bfd3e6
MarcosLazarte <marcos.e.lazarte@gmail.com>
2022-09-09 01:52:07 -0300

    fight

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ async function dameMiPokeDato(poke){
     try {
         const promesa = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
         const pokemon = await promesa.json();
-        console.log(pokemon)
         const sprite = pokemon.sprites.front_default
         const imagen = document.createElement('img');
         imagen.src = sprite;
@@ -26,7 +25,6 @@ async function dameMiPokeDato(poke){
         imagen.alt = "pokemon";
         document.getElementById("otroPoke").append(imagen);
         document.getElementById("nombreEquipoRival").append(pokemon.name)
-        return imagen;
     }
     catch(err) { console.log("Che, hubo un error: ", err)}
 }
@@ -34,13 +32,20 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
   
-const rndInt = randomIntFromInterval(1, 905)
 
-dameMiPokemon(randomIntFromInterval(1, 905));
-dameMiPokeDato(rndInt);
-function holis(){
+let moves1 = dameMiPokemon(randomIntFromInterval(1, 905));
+let moves2 = dameMiPokeDato(randomIntFromInterval(1, 905));
+console.log(moves1)
+function huir(){
     setTimeout( () =>{
         document.getElementById("consola").innerHTML = "No puedes huir en esta version";
+    }, 1000);
+    document.getElementById("consola").innerHTML = "";
+    document.getElementById("consola").append(". . .")
+}
+function pelea(moves1){
+    setTimeout( () =>{
+        document.getElementById("consola").innerHTML = `<div class="menu"><button>${"moves1[0].moves.name"}</button><button>${"moves1[1].moves.name"}</button><button>${"moves1[2].moves.name"}</button><button>${"moves1[3].moves.name"}</button></div>`;
     }, 1000);
     document.getElementById("consola").innerHTML = "";
     document.getElementById("consola").append(". . .")
