ca9f9a0aa94ef6ec7aea0c8f75290050a62432f6
MarcosLazarte <marcos.e.lazarte@gmail.com>
2022-09-14 23:31:42 -0300

    aniumacion de reduccion de vida y lsta de ataques permanente

--- js/main.js
+++ js/main.js
@@ -48,28 +48,68 @@ function pelea(){
     const miPokeId = document.getElementById("miPokeId").innerHTML;
     fetch(`https://pokeapi.co/api/v2/pokemon/${miPokeId}`)
     .then(res => res.json())
-    .then(data => {    
-        console.log(data)
-        let habilidad1 = randomIntFromInterval(1,data.moves.length);
-        let habilidad2 = randomIntFromInterval(1,data.moves.length);
-        let habilidad3 = randomIntFromInterval(1,data.moves.length);
-        let habilidad4 = randomIntFromInterval(1,data.moves.length);
+    .then(data => {
+        
+        //Me aseguro que sí es la primera vez que se busca una habilidad, la guarde en div
+        //Y, que en las continuas llamadas al botón Fight, muestre las mismas habilidades
+        if(document.getElementById("idAtaque1").innerHTML == ""){
 
-        setTimeout( () =>{
-            if(document.getElementById("hud").classList.toggle('hudPelea')){
-                document.getElementById("hud").classList.remove('hudPelea');
-                document.getElementById("hud").classList.add('hudPeleaB')
-            }
-            else{
-                document.getElementById("miPoke").classList.remove('hudPeleaB') 
-                document.getElementById("miPoke").classList.add('hudPelea');
-            }
+            //numero de la habilidad, buscado aleatoriamente
+            let habilidad1 = randomIntFromInterval(1,data.moves.length);    
+            let habilidad2 = randomIntFromInterval(1,data.moves.length);    
+            let habilidad3 = randomIntFromInterval(1,data.moves.length);    
+            let habilidad4 = randomIntFromInterval(1,data.moves.length);
+
+            setTimeout( () =>{
+                if(document.getElementById("hud").classList.toggle('hudPelea')){
+                    document.getElementById("hud").classList.remove('hudPelea');
+                    document.getElementById("hud").classList.add('hudPeleaB')
+                }
+                else{
+                    document.getElementById("miPoke").classList.remove('hudPeleaB') 
+                    document.getElementById("miPoke").classList.add('hudPelea');
+                }
+                document.getElementById("idAtaque1").innerHTML = habilidad1;
+                document.getElementById("idAtaque2").innerHTML = habilidad2;
+                document.getElementById("idAtaque3").innerHTML = habilidad3;
+                document.getElementById("idAtaque4").innerHTML = habilidad4;
+
+                document.getElementById("consola").innerHTML = `<div class="menu">
+                    <button onClick=ataque()>${data.moves[habilidad1].move.name}</button>
+                    <button>${data.moves[habilidad2].move.name}</button>
+                    <button>${data.moves[habilidad3].move.name}</button>
+                    <button>${data.moves[habilidad4].move.name}</button>
+                    </div>`;
+            }, 500);
             
-            document.getElementById("consola").innerHTML = `<div class="menu"><button onClick=ataque()>${data.moves[habilidad1].move.name}</button><button>${data.moves[habilidad2].move.name}</button><button>${data.moves[habilidad3].move.name}</button><button>${data.moves[habilidad4].move.name}</button></div>`;
-        }, 500);
-        document.getElementById("consola").innerHTML = "";
-        document.getElementById("consola").innerHTML = `<div style="font-size:55px">. . .</div>`;
-    })
+            document.getElementById("consola").innerHTML = "";
+            document.getElementById("consola").innerHTML = `<div style="font-size:55px">. . .</div>`;
+        } else {
+            let habilidad1 = document.getElementById("idAtaque1").innerHTML;
+            let habilidad2 = document.getElementById("idAtaque2").innerHTML;
+            let habilidad3 = document.getElementById("idAtaque3").innerHTML;
+            let habilidad4 = document.getElementById("idAtaque4").innerHTML;
+
+            setTimeout( () =>{
+                if(document.getElementById("hud").classList.toggle('hudPelea')){
+                    document.getElementById("hud").classList.remove('hudPelea');
+                    document.getElementById("hud").classList.add('hudPeleaB')
+                }
+                else{
+                    document.getElementById("miPoke").classList.remove('hudPeleaB') 
+                    document.getElementById("miPoke").classList.add('hudPelea');
+                }
+                
+                document.getElementById("consola").innerHTML = `<div class="menu">
+                    <button onClick=ataque()>${data.moves[habilidad1].move.name}<div id="idAtaque1" style="display:none">${habilidad1}</div></button>
+                    <button>${data.moves[habilidad2].move.name}<div id="idAtaque2" style="display:none">${habilidad2}</div></button>
+                    <button>${data.moves[habilidad3].move.name}<div id="idAtaque3" style="display:none">${habilidad3}</div></button>
+                    <button>${data.moves[habilidad4].move.name}<div id="idAtaque4" style="display:none">${habilidad4}</div></button>
+                    </div>`;
+            }, 500);
+            document.getElementById("consola").innerHTML = "";
+            document.getElementById("consola").innerHTML = `<div style="font-size:55px">. . .</div>`;
+    }})
 }
 function abrirEquipo(){
     setTimeout( () =>{
@@ -94,11 +134,36 @@ function ataque(){
         document.getElementById("miPoke").classList.remove('movimientoPegarB') 
         document.getElementById("miPoke").classList.add('movimientoPegar');
     }
+    let ataque = 10;
+    let vida = parseInt(document.getElementById("vidaActual").innerHTML);
+    let vidaTotal = parseInt(document.getElementById("vidaTotal").innerHTML);
+    
+    document.getElementById("vidaActual").innerHTML = vida - ataque;
+
+    addAnimation(`
+      @keyframes animacionBajarVida { 
+         from {
+         }
+        to {
+          width: ${(vida - ataque)*100/vidaTotal}%;
+        }
+      }
+    `);
+
     document.getElementById("vida_barraRoja").classList.add("danio");
 }
 
-document.querySelector('#container').addEventListener('mouseleave', a )
-document.querySelector('#container').addEventListener('mouseenter', b )
+function vida(danio, pokemon){
+    let vidaRestante = document.getElementById("vidaActual").innerHTML;
+    return alert(vidaRestante);
+}
+
+
+
+//Deje estas cosas para verificar cosas
+document.querySelector('#container').addEventListener('mouseleave', a );
+document.querySelector('#container').addEventListener('mouseenter', b );
+
 function a(){
     if(this.classList.toggle('sec')){
         this.classList.toggle('sec');
@@ -110,3 +175,13 @@ function b(){
         this.classList.remove('first')
     }
 }
+function addAnimation(body) {
+    let dynamicStyles = null
+
+    if (!dynamicStyles) {
+      dynamicStyles = document.createElement('style');
+      dynamicStyles.type = 'text/css';
+      document.head.appendChild(dynamicStyles);
+    }
+    dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
+  }
